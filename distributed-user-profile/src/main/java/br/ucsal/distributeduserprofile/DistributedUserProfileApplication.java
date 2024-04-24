95bb7cd34e2a492ad75f2bbb4c5ee33342b53ad6
Paulo <paulo19032004@gmail.com>
2024-04-24 11:32:37 -0300

    Add eureka client user profile

---
+++ distributed-user-profile/src/main/java/br/ucsal/distributeduserprofile/DistributedUserProfileApplication.java
@@ -0,0 +1,15 @@
+package br.ucsal.distributeduserprofile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class DistributedUserProfileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistributedUserProfileApplication.class, args);
+	}
+
+}
