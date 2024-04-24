d8f7f0508299749880a65d51f9f4e4f3cf5e3adb
Paulo <paulo19032004@gmail.com>
2024-04-24 11:30:51 -0300

    Add ISP main application

---
+++ distributed-user-main/src/test/java/br/ucsal/distributedusermain/DistributedUserMainApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ucsal.distributedusermain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributedUserMainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
